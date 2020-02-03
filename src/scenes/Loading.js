2b7310416ae8cac5692b529e511062f7c72faa40
Nick <nicholas.balestrieri@gmail.com>
2020-02-02 23:02:15 -0500

    Improve line timing and positioning for loading screen

--- src/scenes/Loading.js
+++ src/scenes/Loading.js
@@ -2,12 +2,19 @@ import Phaser from "phaser"
 
 export default class Loading extends Phaser.Scene{
     constructor(){
-        super()
-        this.log1 = 'Log 24912: Day 3185\nI did not understand why the humans panicked, \nonly that fear caused them to act in haste, bringing more destruction.'
-        this.log2 = 'I do not understand the words ECOLOGICAL COLAPSE, \nmy purpose was to REPAIR what BROKE.'
-        this.log3 = 'With each day fewer people returned to the FACILITY, \nand there was more to REPAIR with fewer workers.'
-        this.log4 = 'I projected that one day I might BREAK too.'
-        this.log5 = 'One day, the sky outside the FACILITY turned red, \nand no humans returned.'
+        super({
+            key: "loading"
+        })
+        this.log1 = 'Log 24912: Day 3185'
+        this.log2 = 'I did not understand why the humans panicked,' 
+        this.log3 = 'only that fear caused them to act in haste, bringing more destruction.'
+        this.log4 = 'I do not understand the phrase ECOLOGICAL COLAPSE,' 
+        this.log5 = 'my purpose is to REPAIR what BROKE.'
+        this.log6 = 'With each day fewer people returned to the FACILITY,'
+        this.log7 = 'and there was more to REPAIR with fewer workers.'
+        this.log8 = 'I projected that one day I might BREAK too.'
+        this.log9 = 'One day, the sky outside the FACILITY turned red,'
+        this.log10 = 'and no humans returned.'
     }
 
     create(){
@@ -15,29 +22,67 @@ export default class Loading extends Phaser.Scene{
         
         setTimeout(() => {
             this.add.text(50,80, "Complete! Restoring most recent log entry...", {fontSize:"20px"})
-        }, 2000)
+        }, 750)
 
         setTimeout(() => {
             this.add.text(50, 110, 
                 this.log1, 
                 {fontSize:"18px"})
-        }, 3700)
+        }, 1500)
 
         setTimeout(() => {
-            this.add.text(50, 180, this.log2, {fontSize:"18px"})
-        }, 7900)
+            this.add.text(50, 140, 
+                this.log2, 
+                {fontSize:"18px"})
+        }, 2250)
 
         setTimeout(() => {
-            this.add.text(50, 230, this.log3, {fontSize:"18px"})
-        }, 11500)
+            this.add.text(50, 170, 
+                this.log3, 
+                {fontSize:"18px"})
+        }, 3000)
 
         setTimeout(() => {
-            this.add.text(50, 280, this.log4, {fontSize:"18px"})
-        }, 15500)
+            this.add.text(50, 200, 
+                this.log4, 
+                {fontSize:"18px"})
+        }, 3750)
 
         setTimeout(() => {
-            this.add.text(50, 310, this.log5, {fontSize:"18px"})
-        }, 20000)
+            this.add.text(50, 230, 
+                this.log5, 
+                {fontSize:"18px"})
+        }, 4500)
+
+        setTimeout(() => {
+            this.add.text(50, 260, 
+                this.log6, 
+                {fontSize:"18px"})
+        }, 5250)
+
+        setTimeout(() => {
+            this.add.text(50, 290, 
+                this.log7, 
+                {fontSize:"18px"})
+        }, 6000)
+
+        setTimeout(() => {
+            this.add.text(50, 320, 
+                this.log8, 
+                {fontSize:"18px"})
+        }, 6750)
+
+        setTimeout(() => {
+            this.add.text(50, 350, 
+                this.log9, 
+                {fontSize:"18px"})
+        }, 7500)
+
+        setTimeout(() => {
+            this.add.text(50, 380, 
+                this.log10, 
+                {fontSize:"18px"})
+        }, 8250)
 
 	    this.add.text(50, 500, "Press SPACE to skip...", {fontSize:"18px"})
 
